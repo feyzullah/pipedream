e422ac8a5a86cc1e43e42868b3c5925e5c11710b
feyzo <feyzullah.metu@gmail.com>
2022-10-11 21:07:42 +0300

    test3

--- components/zoom/actions/new-action.mjs
+++ components/zoom/actions/new-action.mjs
@@ -1,7 +1,8 @@
+
 export default {
   key: "zoom-new-action",
   name: "asdsad",
   description: "asdasd",
-  version: "asdsad",
+  version: "0.0.1",
   type: "action",
 };
--- components/zoom/gommon2.mjs
+++ components/zoom/gommon2.mjs
@@ -1,3 +1,4 @@
+
 export default {
   a: "b",
   c: "d",
